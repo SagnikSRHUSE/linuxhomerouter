745457e8740d47ac3bc0f93775f91117be258175
Sagnik Sasmal <sagnik@sagnik.me>
2021-10-26 20:19:24 +0530

    mis fixes

--- check_pppoe.sh
+++ check_pppoe.sh
@@ -4,7 +4,7 @@ source ./settings.sh
 
 for iface in $PPPOE_INTERFACES; do
   if ip a | grep $iface &>/dev/null
-    then exit 0
+    then rm $PPPOE_CHECK_FILE
   fi
 done
 
@@ -12,11 +12,9 @@ if [ -f $PPPOE_CHECK_FILE ]
   then echo "[$LOG_TIMESTAMP] PPPoE already being reestablished" >> $LOG_DIR/checker.log && sleep 5 && exit 0
 fi
 
-touch /tmp/check_pppoe
-
 for iface in $PPPOE_INTERFACES; do
   if ! ip a | grep $iface &>/dev/null
-    then pon $iface &>/dev/null && echo "[$LOG_TIMESTAMP] $iface PPPoE detected inactive, attempting to reinitiate" >> $LOG_DIR/checker.log
+    then touch $PPPOE_CHECK_FILE && pon $iface &>/dev/null && echo "[$LOG_TIMESTAMP] $iface PPPoE detected inactive, attempting to reinitiate" >> $LOG_DIR/checker.log
   fi
 done
 
--- settings.sh
+++ settings.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-set -e
-set -o pipefail
-
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
